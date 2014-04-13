e325ef6c789237da255d9599c7360dbd31e0077a
Chris O'Neil <cconeil5@gmail.com>
2014-04-13 06:21:39 -0700

    got it calling every time a key is pressed on the screen

--- kitty.js
+++ kitty.js
@@ -1,15 +1,35 @@
 
 
 
-
-
-// $.keypress()
-
 console.log("kitty");
 
-$.keypress(function(evt) {
-  console.log( "Handler for .keypress() called." );
-});
+document.onkeydown = function(event) {
+  
+
+  
+
+
+
+
+
+
+
+
+
+  // alert('test');
+};
+
+
+function listen()
+
+// $("#container").keydown(function (e) {
+//    //here you decide whether to handle the key event, and grab the control that sent the event 
+//    var myInput = e.target;
+//    $("#container").one('keyup', function() {
+//       console.log(myInput.val());  
+//       // do smth here
+//    });
+// });
 
 
 
